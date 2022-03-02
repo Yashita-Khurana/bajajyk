918fc45751794f26caf4ffbc9d6e1bb2da490a55
Yashita <yashita1109.cse19@chitkara.edu.in>
2022-03-02 14:41:03 +0530

    changes

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 const express = require('express');
-const app = express();
+const app=express();
 app.use(express.json());
 
-app.post("/bfhl",(req,res)=> {
+
+app.post("/bfhl", (req, res) => {
     const {data} = req.body;
-    const numElements = [];
-    const alpha = [];
+    const num = [];
+    const character = [];
     const characterRegex = /^[a-zA-Z]{1,}$/;
     const numberRegex = /^[0-9]{1,}$/;
-
-    data.map((item) => {
-        if(numberRegex.test(item))
-        {
-            numElements.push(item);
-        }
-        else if(characterRegex.test(item))
-        {
-            alpha.push(item);
-        }
-    });
-
-    const result = {
-        isSuccess : "true",
-        user_id : "yashita_khurana_08112000",
-        email : "yashita1109.cse19@chitkara.edu.in",
-        roll_number : "1910991109",
-        numbers : numElements ,
-        alphabets : alpha
-    };
-
-    res.status(200).send(result);
-
+    
+        data.map((item) => {
+            if(numberRegex.test(item)) {
+                num.push(item);
+            } else if(characterRegex.test(item)){
+                character.push(item);
+            }
+        });
+    
+        const result = {
+            isSuccess: "true",
+            user_id: "yashita_khurana_08112000",
+            email: "yashita1109.cse19@chitkara.edu.in",
+            roll_number:"1910991109" ,
+            numbers: num,
+            alphabets: character
+        };
+    
+        res.status(200).send(result);
+    
 });
 
 const port = 8000;
 app.listen(port,()=> {
     console.log(`app running on port ${port}`);
-});
+})
